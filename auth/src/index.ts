f40ccee2364129573b88c9a142b3645f693aa6d2
Travis Gerrard <travisgerrard@gmail.com>
2022-06-30 20:02:23 -0700

    got rid of logs

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,9 +3,6 @@ import mongoose from 'mongoose';
 import { app } from './app';
 
 const start = async () => {
-  console.log('!!');
-  console.log('!!');
-
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
